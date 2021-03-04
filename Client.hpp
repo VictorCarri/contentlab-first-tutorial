587df4b1a80563333f7d5429888312ea515901d8
Victor Carri <victorcarri@gmail.com>
2021-03-04 18:29:24 -0500

    Added an io_context for Asio ops.

--- Client.hpp
+++ Client.hpp
@@ -1,10 +1,16 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+/* Boost */
+#include <boost/asio/io_context.hpp> // boost::asio::io_context
+
 class Client
 {
 	public:
+		Client();
+
 	private:
+		boost::asio::io_context ioc; // Needed by Boost.Asio for all operations.
 };
 
 #endif
