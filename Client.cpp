1001ced20762b1979c4d64f142d7f61aac778e58
Victor Carri <victorcarri@gmail.com>
2021-03-04 22:37:22 -0500

    Added code to initialize the resolver.

--- Client.cpp
+++ Client.cpp
@@ -1,5 +1,7 @@
 #include "Client.hpp"
 
-Client::Client() : ioc() // Create our I/O context
+Client::Client() : ioc() // Create our I/O context,
+	resolver(ioc), // Initialize the resolver
+	socket(ioc) // Initialize the socket
 {
 }
